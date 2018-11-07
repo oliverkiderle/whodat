2a437a7954a78d1d2213db613d1dc848aaa80bd6
jvdheyden <jonasvdheyden@gmail.com>
2018-11-07 19:11:04 +0100

    started building parser

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 import requests
 import json
 import urllib
+import re
 
 personality = 'Karl Marx'
 urllib.quote(personality)
@@ -10,5 +13,37 @@ r = requests.get('https://en.wikipedia.org/w/api.php?format=json'
                  '&redirects=1&titles=%s' % personality)
 
 j = json.loads(r.text)
-print json.dumps(j['query']['pages']['16743']['extract'],
+bio = json.dumps(j['query']['pages']['16743']['extract'],
                  sort_keys=True, indent=4, separators=(',', ': '))
+
+names = personality.split()
+genitiv = [x + "'s" for x in names] + [x + "'" for x in names]
+gendered_pronouns = ['he','she', 'He', 'She']
+
+possessive = ['his', 'her', 'His', 'Her'] + genitiv
+
+replaceables = [personality] + names + gendered_pronouns
+
+for i in replaceables:
+    bio = re.sub('\"' + i, 'This person', bio)
+    
+    p = re.compile('(\. )' + i)
+    bio = p.sub(r'\1They', bio)
+
+    p = re.compile('(\.)' + i)
+    bio = p.sub(r'\1 They', bio)
+
+    p = re.compile('(\s)' + i + '[ ,\n]' )
+    bio = p.sub(r'\1they ', bio)
+
+for i in possessive:
+    p = re.compile('(\. )' + i)
+    bio = p.sub(r'\1Their', bio)
+
+    p = re.compile('(\.)' + i)
+    bio = p.sub(r'\1Their', bio)
+
+    p = re.compile('(\s)' + i + ' ')
+    bio = p.sub(r'\1their ', bio)
+
+print bio
